73195ed06671c95427926651f38eccaa57c16529
loshido <loshido@pm.me>
2025-08-11 19:08:23 +0200

    Filter branches for webhook

--- src/pages/github.ts
+++ src/pages/github.ts
@@ -5,16 +5,18 @@ import { verifySignature } from "../services/git.ts";
 const GITHUB_WEBHOOK = Deno.env.get('GITHUB_WEBHOOK')
 if(!GITHUB_WEBHOOK) throw new Error('GITHUB_WEBHOOK has not been set')
 
+const UPDATE_BRANCH = Deno.env.get('UPDATE_BRANCH') || 'production'
+
 export const POST: APIRoute = async  ({ request }) => {
-    const event = request.headers.get('X-Github-Event')
-    if(event !== 'push') return new Response(null, { status: 400 })
-        
     const body = await request.json()
+    
+    const event = request.headers.get('X-Github-Event')
+    if(event !== 'push' || body.ref !== 'refs/heads/' + UPDATE_BRANCH) return new Response(null, { status: 400 })
+        
     const signature = request.headers.get('X-Hub-Signature-256')?.split('=').at(1)
     if(!signature || !(await verifySignature(GITHUB_WEBHOOK, signature, JSON.stringify(body)))) {
         return new Response(null, { status: 401 })
     }
-
 
     const time = new Date().toLocaleTimeString('fr-FR', { timeStyle: 'medium' })
     console.log(`%c${time}%c [Webhook] Push event (${ body.pusher.name })`, 'color: #0004', 'color: #fff')
