991bddf9982be46beea2a2cd5bcbda94742d1f82
Loshido <loshido@pm.me>
2026-02-16 16:00:03 +0100

    cache-control headers

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,23 @@
-use axum::Router;
+use axum::{Router, extract::Request, middleware::{self, Next}, response::Response};
 use tower_http::services::ServeDir;
 
-mod macros;
 mod net;
+
+async fn cache_control(mut req: Request, next: Next) -> Response {
+    let headers = req.headers_mut();
+
+    headers.insert("cache-control", "public, max-age=2592000, immutable".parse().unwrap());
+
+    next.run(req).await
+}
 
 #[tokio::main]
 async fn main() {
     let dist = ServeDir::new("./dist");
 
     let app = Router::new()
-        .fallback_service(dist);
+        .fallback_service(dist)
+        .layer(middleware::from_fn(cache_control));
 
     net::serve(app).await;
 }
